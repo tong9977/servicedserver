eba2aaa77f5be9bc6ca5fc3ec81d2a833fd9d61d
suriyapong <suriyapong_s@hotmail.com>
2019-04-25 10:50:04 +0700

    Delete Server & Server 3

--- server3/src/services/index.js
+++
@@ -1,9 +0,0 @@
-const company = require('./company/company.service.js');
-const vehicles = require('./vehicles/vehicles.service.js');
-const big = require('./big/big.service.js');
-// eslint-disable-next-line no-unused-vars
-module.exports = function (app) {
-  app.configure(company);
-  app.configure(vehicles);
-  app.configure(big);
-};
